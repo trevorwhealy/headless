c3a1b8d80249b7fb9a343598d869250f94e28abf
Trevor Healy <trevorhealy@Trevors-MacBook-Pro.local>
2017-10-09 22:05:22 -0700

    fixup

--- android/app/src/main/java/com/headless/receiver/MyReceiver.java
+++ android/app/src/main/java/com/headless/receiver/MyReceiver.java
@@ -3,43 +3,28 @@ package com.headless.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-
 import com.headless.service.MyTaskService;
-
 import com.facebook.react.HeadlessJsTaskService;
-import android.telephony.TelephonyManager;
-
-import android.net.ConnectivityManager;
-import android.app.ActivityManager;
-import java.util.List;
-import android.net.NetworkInfo;
-
 import android.os.Bundle;
 import android.telephony.SmsMessage;
-
 import android.util.Log;
-
-import android.support.v4.content.WakefulBroadcastReceiver;
 import android.widget.Toast;
-
 import com.headless.MainActivity;
 
 public class MyReceiver extends BroadcastReceiver {
-
-    public static final String SMS_BUNDLE = "pdus";
 
     public void onReceive(Context context, Intent intent) {
         Bundle intentExtras = intent.getExtras();
         SmsMessage [] messages = null;
         String strMessage = "";
 
-
-        Log.w("ABCCC", "DEFFGGG");
+        Log.w("onReceiveInvoked", "onReceiveInvoked");
 
             if (intentExtras != null) {
+              Log.w("intentExtrasNotNull", "intentExtrasNotNull");
+
               Object [] pdus = (Object[]) intentExtras.get("pdus");
               messages = new SmsMessage[pdus.length];
-
               for (int i = 0; i < messages.length; i++) {
                   messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
                   strMessage += "SMS From: " + messages[i].getOriginatingAddress();
@@ -48,21 +33,20 @@ public class MyReceiver extends BroadcastReceiver {
                   strMessage += "\n";
               }
 
-              Log.w("WHY", "THOOOOOOOOO");
-
               Toast.makeText(context, "Message Received!", Toast.LENGTH_SHORT).show();
 
-              if (MainActivity.active) {
-//                  MainActivity inst = MainActivity.instance();
-//                  inst.updateInbox(strMessage);
-              } else {
+              // context.startService(i);
+              // HeadlessJsTaskService.acquireWakeLockNow(context);
+
+              if (!MainActivity.active) {
                   Intent i = new Intent(context, MyTaskService.class);
-                  i.putExtra("hasInternet", "fucking idk");
-                  // i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                  context.getApplicationContext().startService(i);
-                  // context.startService(i);
+                  i.putExtra("SMS:", strMessage);
+                  // context.getApplicationContext().startService(i);
+                  context.startService(i);
                   HeadlessJsTaskService.acquireWakeLockNow(context);
               }
+            } else {
+              Log.w("intentExtrasISNull", "intentExtrasISNull");
             }
     }
 }
